29d69565bb07008ef372c71a3dc593046192399d
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-05-14 20:42:18 -0700

    Initial commit

---
+++ MoveLimiter/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using MelonLoader;
+using MoveLimiter; // The namespace of your mod class
+using System.Reflection;
+using System.Runtime.InteropServices;
+// ...
+[assembly: MelonInfo(typeof(main), "MoveLimiter", "1.0.2", "UlvakSkillz")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MoveLimiter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoveLimiter")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b1f33f41-50c5-4621-b6e4-55df6d828814")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MoveLimiter/main.cs
@@ -0,0 +1,123 @@
+﻿using MelonLoader;
+using RUMBLE.Managers;
+using RUMBLE.Players.Subsystems;
+using RUMBLE.Poses;
+using System.Collections;
+using UnityEngine;
+
+namespace MoveLimiter
+{
+    public class main : MelonMod
+    {
+        private bool sceneChanged = false;
+        private string currentScene = "";
+        int sceneChangeCount = 0;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            currentScene = sceneName;
+            sceneChanged = true;
+            sceneChangeCount++;
+        }
+
+        public override void OnFixedUpdate()
+        {
+            if (sceneChanged)
+            {
+                if ((currentScene == "Map0") || (currentScene == "Map1"))
+                {
+                    MelonCoroutines.Start(WaitASecondBeforePlayerCheck(sceneChangeCount));
+                }
+                sceneChanged = false;
+            }
+        }
+
+        public IEnumerator WaitASecondBeforePlayerCheck(int sceneCount)
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                yield return new WaitForFixedUpdate();
+            }
+            while (PlayerManager.instance.localPlayer.Controller == null)
+            {
+                yield return new WaitForFixedUpdate();
+            }
+            bool enabled = PlayerManager.instance.localPlayer.Controller.gameObject.transform.GetComponentInChildren<PlayerPoseSystem>().enabled;
+            while (!enabled)
+            {
+                if (sceneCount != sceneChangeCount) { break; }
+                enabled = PlayerManager.instance.localPlayer.Controller.gameObject.transform.GetComponentInChildren<PlayerPoseSystem>().enabled;
+                yield return new WaitForFixedUpdate();
+            }
+            if (enabled)
+            {
+                if (PlayerManager.instance.AllPlayers.Count == 2)
+                {
+                    ChangeAvailableMoves(PlayerManager.instance.AllPlayers[1].Data.GeneralData.BattlePoints);
+                }
+            }
+        }
+
+        public void ChangeAvailableMoves(int opponentBP)
+        {
+            if ((opponentBP >= 156) || (opponentBP >= PlayerManager.instance.localPlayer.Data.GeneralData.BattlePoints)) { return; }
+            System.Collections.Generic.List<string> movesToKeep = new System.Collections.Generic.List<string>();
+            string beltRankFound = "White Belt";
+            movesToKeep.Add("PoseSetDisc");
+            movesToKeep.Add("PoseSetSpawnPillar");
+            movesToKeep.Add("PoseSetStraight");
+            movesToKeep.Add("SprintingPoseSet");
+            if (opponentBP >= 12)
+            {
+                beltRankFound = "Yellow Belt";
+                movesToKeep.Add("PoseSetBall");
+                movesToKeep.Add("PoseSetKick");
+                movesToKeep.Add("PoseSetStomp");
+            }
+            if (opponentBP >= 30)
+            {
+                beltRankFound = "Green Belt";
+                movesToKeep.Add("PoseSetWall_Grounded");
+                movesToKeep.Add("PoseSetRockjump");
+                movesToKeep.Add("PoseSetUppercut");
+            }
+            if (opponentBP >= 54)
+            {
+                beltRankFound = "Blue Belt";
+                movesToKeep.Add("PoseSetSpawnCube");
+                movesToKeep.Add("PoseSetDash");
+            }
+            if (opponentBP >= 96)
+            {
+                beltRankFound = "Red Belt";
+                movesToKeep.Add("PoseSetParry");
+                movesToKeep.Add("PoseSetHoldLeft");
+                movesToKeep.Add("PoseSetHoldRight");
+            }
+            Il2CppSystem.Collections.Generic.List<PoseInputSource> activePoses = PlayerManager.instance.localPlayer.Controller.gameObject.transform.GetComponentInChildren<PlayerPoseSystem>().currentInputPoses;
+            string poseList = "";
+            for (int i = 0; i < activePoses.Count; i++)
+            {
+                bool poseFound = false;
+                for (int x = 0; x < movesToKeep.Count; x++)
+                {
+                    if (activePoses[i].poseSet.name == movesToKeep[x])
+                    {
+                        poseFound = true;
+                    }
+                }
+                if (!poseFound)
+                {
+                    if (poseList != "")
+                    {
+                        poseList += ", ";
+                    }
+                    poseList += activePoses[i].poseSet.name;
+                    activePoses.RemoveAt(i);
+                    i--;
+                }
+            }
+            MelonLogger.Msg($"Facing {beltRankFound}, Removing Poses: {poseList}");
+        }
+    }
+}
